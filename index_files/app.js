45667c2d60bca67dffd0e1afba704997a7c6a3e9
maxencestievenard <shosniping@gmail.com>
2023-01-01 18:35:17 +0100

    insh
    insh ca marche

---
+++ index_files/app.js
@@ -0,0 +1,45 @@
+window.onscroll = function (){
+    if(document.documentElement.scrollTop>1000){
+        document.getElementById("myNavbar").style.padding="0px 0px"
+    }
+    else {
+        document.getElementById("myNavbar").style.padding ="25px 10px";
+    }
+
+}
+
+
+window.onload = function load() {
+    if (window.innerWidth < 757) {
+        document.getElementById("panel").style.display = "none";
+        document.getElementById("panel2").style.display = "block";
+        document.getElementById("panel3").style.display = "none";
+        document.getElementById("panel4").style.display = "block";
+
+    } else {
+        document.getElementById("panel").style.display = "block";
+        document.getElementById("panel2").style.display = "none";
+        document.getElementById("panel3").style.display = "block";
+        document.getElementById("panel4").style.display = "none";
+    }
+}
+window.onresize = resize;
+function resize()
+{
+    if(window.innerWidth < 757){
+        document.getElementById("panel").style.display = "none";
+        document.getElementById("panel2").style.display = "block";
+        document.getElementById("panel3").style.display = "none";
+        document.getElementById("panel4").style.display = "block";
+    }
+
+    else{
+        document.getElementById("panel").style.display = "block";
+        document.getElementById("panel2").style.display = "none";
+        document.getElementById("panel3").style.display = "block";
+        document.getElementById("panel4").style.display = "none";
+    }
+
+
+
+}
